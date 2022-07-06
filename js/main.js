01ba8fa5de246b470d4a7035ba807c63f210b13b
Marco Valerio <mo.valerio93@gmal.com>
2022-07-06 18:41:21 +0200

    Treno form html structture js data

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementById('genrate').addEventListener('click',function(){
+
+    const numeroKm = parseInt( document.getElementById('km').value);
+    const eta = parseInt( document.getElementById('age').value);
+    const costoKm = 0.21;
+}
+
+
+
+)
